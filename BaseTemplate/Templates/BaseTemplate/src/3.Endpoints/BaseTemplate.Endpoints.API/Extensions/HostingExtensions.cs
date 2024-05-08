7fe3728beb15b4d59ecf45fee87224dc74519448
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-05-08 22:42:29 +0330

    edit setting

--- BaseTemplate/Templates/BaseTemplate/src/3.Endpoints/BaseTemplate.Endpoints.API/Extensions/HostingExtensions.cs
+++ BaseTemplate/Templates/BaseTemplate/src/3.Endpoints/BaseTemplate.Endpoints.API/Extensions/HostingExtensions.cs
@@ -15,58 +15,23 @@ public static class HostingExtensions
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
         IConfiguration configuration = builder.Configuration;
-
-        //Base
         builder.Services.AddBaseApiCore("Base", "BaseTemplate");
-
-        //microsoft
         builder.Services.AddEndpointsApiExplorer();
-
-        //Base
         builder.Services.AddBaseWebUserInfoService(configuration, "WebUserInfo", true);
-
-        //Base
         builder.Services.AddBaseParrotTranslator(configuration, "ParrotTranslator");
-
-        //Base
-        //builder.Services.AddSoftwarePartDetector(configuration, "SoftwarePart");
-
-        //Base
         builder.Services.AddNonValidatingValidator();
-
-        //Base
         builder.Services.AddBaseMicrosoftSerializer();
-
-        //Base
         builder.Services.AddBaseAutoMapperProfiles(option =>
         {
-            option.AssemblyNamesForLoadProfiles = "Base";
+            option.AssemblyNamesForLoadProfiles = "BaseTemplate";
         });
 
-
-        //Base
         builder.Services.AddBaseInMemoryCaching();
-        //builder.Services.AddBaseSqlDistributedCache(configuration, "SqlDistributedCache");
-
-        //CommandDbContext
         builder.Services.AddDbContext<DbContextNameCommandDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("CommandDb_ConnectionString"))
             .AddInterceptors(new SetPersianYeKeInterceptor(), new AddAuditDataInterceptor()));
-
-        //QueryDbContext
         builder.Services.AddDbContext<DbContextNameQueryDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("QueryDb_ConnectionString")));
-
-        //PollingPublisher
         builder.Services.AddBasePollingPublisherDalSql(configuration, "PollingPublisherSqlStore");
-        //builder.Services.AddBasePollingPublisher(configuration, "PollingPublisher");
-
-        //MessageInbox
         builder.Services.AddBaseMessageInboxDalSql(configuration, "MessageInboxSqlStore");
-        //builder.Services.AddBaseMessageInbox(configuration, "MessageInbox");
-
-        //builder.Services.AddBaseRabbitMqMessageBus(configuration, "RabbitMq");
-
-        //builder.Services.AddBaseTraceJeager(configuration, "OpenTeletmetry");
-
         builder.Services.AddSwaggerGen();
 
         return builder.Build();
@@ -74,41 +39,22 @@ public static class HostingExtensions
 
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
-        //Base
         app.UseBaseApiExceptionHandler();
-
-        //Serilog
         app.UseSerilogRequestLogging();
-
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
         app.UseStatusCodePages();
-
         app.UseCors(delegate (CorsPolicyBuilder builder)
         {
             builder.AllowAnyOrigin();
             builder.AllowAnyHeader();
             builder.AllowAnyMethod();
         });
-
         app.UseHttpsRedirection();
-
-        //app.Services.ReceiveEventFromRabbitMqMessageBus(new KeyValuePair<string, string>("MiniAggregateName", "AggregateNameCreated"));
-
-        //var useIdentityServer = app.UseIdentityServer("OAuth");
-
         var controllerBuilder = app.MapControllers();
-
-        //if (useIdentityServer)
-        //    controllerBuilder.RequireAuthorization();
-
-        //app.Services.GetService<SoftwarePartDetectorService>()?.Run();
-
         return app;
     }
 }
